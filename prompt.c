a2ab0dfed63005359fa14a593092f5570deda141
Kehindeadebisi <Kehindeadebisi95@gmail.com>
2021-11-03 04:12:59 -0700

    added a betty compliant code

---
+++ prompt.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *main - getsline
+ *Return: 0
+ */
+
+
+int main(void)
+{
+	char *buffer;
+	size_t buff_size = 1024;
+
+	buffer = malloc(buff_size * sizeof(char));
+	if (buffer == NULL)
+	{
+		perror("Cannot allocate memory");
+	}
+	printf("$ ");
+
+	while (buffer)
+	{
+		if (feof(stdin))
+		{
+			break;
+		}
+		getline(&buffer, &buff_size, stdin);
+		printf("%s", buffer);
+	}
+}
