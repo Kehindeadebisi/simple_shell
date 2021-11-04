881a882dbdf6d8be6480f60f4e29ca4869bb6ea7
dk-Gk <idkin19@gmail.com>
2021-11-04 03:07:59 +0000

    prompt

--- prompt.c
+++ prompt.c
@@ -1,33 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+#include <main.h>
 /**
  *main - getsline
  *Return: 0
  */
 
 
-int main(void)
+void print_prompt(void)
 {
-	char *buffer;
-	size_t buff_size = 1024;
-
-	buffer = malloc(buff_size * sizeof(char));
-	if (buffer == NULL)
-	{
-		perror("Cannot allocate memory");
-	}
-	printf("$ ");
-
-	while (buffer)
-	{
-		if (feof(stdin))
-		{
-			break;
-		}
-		getline(&buffer, &buff_size, stdin);
-		printf("%s", buffer);
-	}
-	return (0);
+	write(1, "#cisfun$ ",10);
 }
